3585d202f1a0567273b7546e86ac101e0c2f1a2c
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-09 01:42:41 +0600

    added link color

--- pages/Heros/index.jsx
+++ pages/Heros/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const index = () => {
     return <div>
         <h1>This is a Nested Hero page</h1>
-        <Link href="Heros/Hero">Open Hero</Link>
+        <Link href="Heros/Hero" passHref><span className="text-blue-700 underline cursor-pointer">Open Hero</span></Link>
     </div>;
 };
 
