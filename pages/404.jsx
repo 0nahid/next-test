6f120ce17d55133a08dc303c8efa67b6d648bc40
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-09 02:01:36 +0600

    redirect user to home if they visit any wrong url

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,13 @@
-import Link from 'next/link'
-
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 export default function NotFound() {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 5000);
+    }, [router])
     return (
         <>
             <div className="text-center">
