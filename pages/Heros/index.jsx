37ca73aa0c13ee31c01dc269823eca501851470c
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-12 05:48:10 +0600

    added dynamic routes

--- pages/Heros/index.jsx
+++ pages/Heros/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from 'next/link';
 import React from 'react';
 
 export const getStaticProps = async () => {
@@ -22,18 +23,19 @@ const index = ({ heros }) => {
             {/* Users data */}
             <div>
                 <h1 className="text-4xl text-[#009B5A]">All Heros</h1>
-                {heros.map(ninja => (
-                    <div key={ninja.id} className="mt-4 bg-slate-100 p-4 rounded hover:border-l-4 hover:border-[#009b5a]">
-                        <h3 className="text-1xl">{ninja.name}</h3>
-                    </div>
+                {heros.map(hero => (
+                    <Link href={`Heros/${hero.id}`} key={hero.id} passHref >
+                        <div className="mt-4 bg-slate-100 p-4 rounded cursor-pointer hover:border-l-4 hover:border-[#009b5a]">
+                            <a className="text-1xl ">{hero.name}</a>
+                        </div>
+                    </Link>
                 ))}
             </div>
 
             {/* <div style={{ margin: '20px' }} >
             <Image src="/hero.svg" layout="responsive" width={700} height={300} alt="Hero" />
         </div> */}
-            {/* <Link href="Heros/Hero" passHref><span className="text-[#009B5A] underline cursor-pointer">Open Hero</span></Link> */}
-
+            <Link href="Heros/Hero" passHref><span className="text-[#009B5A] underline cursor-pointer">Open Hero</span></Link>
         </>
     )
 };
